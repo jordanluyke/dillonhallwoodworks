4da1014715556be8ee6a18f77173e7412013e4fc
Jordan Luyke <jordanluyke@gmail.com>
2019-12-18 17:40:02 -0700

    fix favicon security issue

--- client/src/app/app.routing.ts
+++ client/src/app/app.routing.ts
@@ -25,4 +25,4 @@ const routes: Routes = [
     }
 ]
 
-export const routing: ModuleWithProviders = RouterModule.forRoot(routes, {useHash: true})
+export const routing: ModuleWithProviders = RouterModule.forRoot(routes)
