fde0173c4645e32f24d1182518da5654756df650
Jordan Luyke <jordan@jordanluyke.com>
2021-02-09 22:49:24 -0700

    update packages

--- client/src/app/app.routing.ts
+++ client/src/app/app.routing.ts
@@ -21,4 +21,4 @@ const routes: Routes = [
     }
 ]
 
-export const routing: ModuleWithProviders = RouterModule.forRoot(routes)
+export const routing: ModuleWithProviders<RouterModule> = RouterModule.forRoot(routes)
